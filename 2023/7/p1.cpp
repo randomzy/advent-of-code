cab0b54e1e6fd8968aebe85260736ac2ae10cfbd
Martin Tanchev <m1tanchev@gmail.com>
2024-03-28 00:17:01 +0200

    add solutions for day 7

---
+++ 2023/7/p1.cpp
@@ -0,0 +1,83 @@
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <array>
+#include <numeric>
+#include <algorithm>
+
+int ctoid(char c)
+{
+    int id = 0;
+    switch (c) {
+        case 'T':
+            id = 8;
+            break;
+        case 'J':
+            id = 9;
+            break;
+        case 'Q':
+            id = 10;
+            break;
+        case 'K':
+            id = 11;
+            break;
+        case 'A':
+            id = 12;
+            break;
+        default:
+            id = c - '2';
+    }
+    return id;
+}
+
+struct B
+{
+    char hand[6];
+    int bid;
+    std::array<int, 13> cnt;
+    std::array<int, 13> ids;
+    bool operator < (const B & other)
+    {
+        // this < other
+        int i = 0;
+        while (cnt[ids[i]] == other.cnt[other.ids[i]] && cnt[ids[i]] != 0 && other.cnt[other.ids[i]] !=0) {i++;}
+        if (cnt[ids[i]] < other.cnt[other.ids[i]]) {
+            return true;
+        } else if (cnt[ids[i]] == other.cnt[other.ids[i]]) {
+            int j = 0;
+            while(ctoid(hand[j]) == ctoid(other.hand[j])) {j++;}
+            return ctoid(hand[j]) < ctoid(other.hand[j]);
+        } else {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    std::vector<B> bids;
+    B tmp;
+    tmp.cnt.fill(0);
+    std::iota(tmp.ids.begin(), tmp.ids.end(), 0);
+    while(scanf("%s %d\n", tmp.hand, &tmp.bid) == 2) {
+        bids.push_back(tmp);
+    }
+    for (auto & b: bids) {
+        for (int i = 0; i < 5; i++) {
+            b.cnt[ctoid(b.hand[i])]++;
+        }
+    }
+    for (auto & bid: bids) {
+        std::stable_sort(bid.ids.begin(), bid.ids.end(), [&bid](int a, int b){
+            return bid.cnt[a] > bid.cnt[b];
+        });
+    }
+    std::sort(bids.begin(), bids.end());
+    uint64_t res = 0;
+    for (int i = 0; i < bids.size(); i++) {
+        res += bids[i].bid * (i+1);
+        std::cout << bids[i].hand << std::endl;
+    }
+    std::cout << res << std::endl;
+}
